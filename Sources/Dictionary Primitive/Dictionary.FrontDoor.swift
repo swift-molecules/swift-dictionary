9df05269be53707dc05385b63f840457aa9ac7e0
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 20:06:16 +0200

    admin wip

--- Sources/Dictionary Primitive/Dictionary.FrontDoor.swift
+++ Sources/Dictionary Primitive/Dictionary.FrontDoor.swift
@@ -12,7 +12,7 @@
 public import Buffer_Linear_Primitive
 public import Buffer_Primitive
 public import Hash_Indexed_Primitive
-public import Hash_Primitives
+import Hash_Primitives
 public import Memory_Allocator_Primitive
 public import Memory_Heap_Primitives
 public import Storage_Contiguous_Primitives
